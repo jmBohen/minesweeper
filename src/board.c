f1e9f0481e69e8e4516e31cea76f34f7a3e3f333
Jan Bochenski <jmbochenski@gmail.com>
2024-12-25 18:14:51 +0100

    recursive squares unreaveling and function returning list of neighbours

--- src/board.c
+++ src/board.c
@@ -24,6 +24,48 @@ Board create_empty_board(){
 }
 
 
+int** get_neighbours(Board board, int r, int c){
+    //List:
+
+    // s0 s1 s2
+    // s3 rc s4
+    // s5 s6 s7
+
+    //s0 s1 s2 s3 s4 s5 s6 s7
+    //r0 r1 r2 r3 r4 r5 r6 r7
+    //c0 c1 c2 c3 c4 c5 c6 c7
+
+    int **neighbours = malloc(9*sizeof(int*));
+    for (int i = 0; i < 9; i++){
+        neighbours[i] = malloc(2 * sizeof(int));
+    }
+
+    int neighbour_counter = 0;
+    for (int r_piovt = -1; r_piovt <= 1; r_piovt++){
+        for (int c_pivot = -1; c_pivot <= 1; c_pivot++){
+            
+            if(r_piovt == 0 && c_pivot ==0){
+                continue;
+            }
+
+            if( r + r_piovt >= 0 &&
+                r + r_piovt < board-> size_c &&
+                c + c_pivot >= 0 &&
+                c + c_pivot < board-> size_r){
+                    neighbours[neighbour_counter][0] = r + r_piovt;
+                    neighbours[neighbour_counter][1] = c + c_pivot;
+                    neighbour_counter++;
+                }
+            
+        }
+    }
+    neighbours[neighbour_counter][0] = EOF;
+    neighbours[neighbour_counter][1] = EOF;
+
+    return neighbours;
+}
+
+
 // setting up the game settings: board size, number of mines, time
 void set_settings(Board board){
     char symbol;
@@ -35,6 +77,7 @@ void set_settings(Board board){
         {
         //setting up default small board
         case SMALL_SYMBOL:
+            while(getchar() != '\n');
             board -> size_c = SMALL_BOARD;
             board -> size_r = SMALL_BOARD;
             board -> number_of_mines = SMALL_NUMBER_OF_MINES;
@@ -42,6 +85,7 @@ void set_settings(Board board){
             return;
         //setting up default medium board
         case MEDIUM_SYMBOL:
+            while(getchar() != '\n');
             board -> size_c = MEDIUM_BOARD;
             board -> size_r  = MEDIUM_BOARD;
             board -> number_of_mines = MEIDUM_NUMBER_OF_MINES;
@@ -49,6 +93,7 @@ void set_settings(Board board){
             return;
         //setting up default large board
         case LARGE_SYMBOL:
+            while(getchar() != '\n');
             board -> size_c = LARGE_BOARD_C;
             board -> size_r = LARGE_BOARD_R;
             board -> number_of_mines = LARGE_NUMBER_OF_MINES;
@@ -57,6 +102,7 @@ void set_settings(Board board){
 
         //setting up custom board
         case CUSTOM_SYMBOL:
+            while(getchar() != '\n');
             int number;
             while(1){
                 printf(CUSTOM_SETTINGS_QUERY_R);
@@ -99,6 +145,7 @@ void set_settings(Board board){
             return;
 
         default:
+            while(getchar() != '\n');
             printf("Please enter correct input!\n");
             break;
         }
@@ -106,14 +153,40 @@ void set_settings(Board board){
     
 }
 
+
+//on square click
+void reveal_square(Board board, int r, int c){
+    
+    //if already revealed abort
+    if (board -> squares[r * board -> size_r + c] -> is_revealed) return;
+    //do not reveal if square is flagged
+    if (board -> squares[r * board -> size_r + c] -> is_flagged) return;
+
+    //set square as revealed
+    board -> squares[r * board -> size_r + c] -> is_revealed = 1;
+
+    //if mine then game over
+    if (board -> squares[r * board -> size_r + c] -> is_mine){
+        printf("\nGAME OVER\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    //if doesnt have neighbouring mines - reveal neighbour squares
+    if (!(board -> squares[r * board -> size_r + c] -> number_of_neighbour_mines)){
+        int **neighbours = get_neighbours(board, r, c);
+        for (int i = 0; neighbours[i][0] != EOF; i++){
+            reveal_square(board, neighbours[i][0], neighbours[i][1]);
+        } 
+    }
+}
+
+
 //sets up board after first click
 void initialize_board(Board board, int r, int c){
-    //marking first clicked square as revealed
-    board -> squares[r * board -> size_r + c] -> is_revealed = 1;
     
     //randomly selecting mines coordinates 
     //avoiding:
-    //- first clicked square
+    //- first clicked square and its neighbours
     //- setting mine twice(correct at the same place
     int correctly_added_mines = 0;
     srand(time(0));
@@ -126,18 +199,72 @@ void initialize_board(Board board, int r, int c){
         //skipping already set mines
         if(board -> squares[rand_r * board -> size_r + rand_c] -> is_mine) continue;
 
-        //skipping first clicked square
-        else if(rand_r == r && rand_c == c) continue;
+        //skipping first clicked square and its neighbours
+        else if((rand_r == r && rand_c == c)||
+                (rand_r == r - 1 && rand_c == c-1)||
+                (rand_r == r - 1 && rand_c == c)||
+                (rand_r == r - 1 && rand_c == c +1)||
+                (rand_r == r && rand_c == c - 1)||
+                (rand_r == r && rand_c == c + 1)||
+                (rand_r == r + 1 && rand_c == c - 1)||
+                (rand_r == r + 1 && rand_c == c)||
+                (rand_r == r + 1 && rand_c == c + 1)) continue;
         else{
             board -> squares[rand_r * board -> size_r + rand_c] -> is_mine = 1;
             correctly_added_mines++;
             increase_number_of_neighbour_mines_for_neighbours(board, rand_r, rand_c);
         }
     }
+
+    //reveal square
+    reveal_square(board, r, c);
+    //board -> squares[r * board -> size_r + c] -> is_revealed = 1;
 }
 
-// prints board as matrix
+void increase_number_of_neighbour_mines_for_neighbours(Board board, int r, int c){
+
+    int **neighbours = get_neighbours(board, r, c);
+
+    //increase number of surrounding mines for each neighbour
+    for (int i = 0; neighbours[i][0] != EOF; i++){
+        board -> squares[neighbours[i][0] * board -> size_r + neighbours[i][1]] -> number_of_neighbour_mines += 1;
+    }
+}
+
+
+// prints board as covered matrix
 void print_board(Board board){
+    
+    printf("||");
+    for (int i = 0; i < board -> size_r; i++){printf("=");}
+    printf("||\n");
+
+    for (int y = 0; y < board -> size_c; y++){
+        printf("||");
+        for (int x = 0; x < board -> size_r; x++){
+            Square square = board -> squares[y* board -> size_r + x];
+
+            //if is flagged - means you cannot flag revealed square
+            if (square -> is_flagged) printf("%c", FLAG_CHAR);
+            //if just unrevealed 
+            else if (!square -> is_revealed) printf("%c", UNREVEALED_CHAR);
+            //if is revealed  and mine
+            else if (square -> is_mine) printf("%c", MINE_CHAR);
+            //if revealed and has no neighbour mines
+            else if (square -> number_of_neighbour_mines == 0) printf("%c", REVEALED_CLEAR_CHAR);
+            //if revealed and has any neighbour mines
+            else printf("%d", square -> number_of_neighbour_mines);
+
+        }
+        printf("||\n");
+    }
+
+    printf("||");
+    for (int i = 0; i < board -> size_r; i++){printf("=");}
+    printf("||\n");
+}
+// prints board as uncovered matrix
+void dev_print_board(Board board){
     
     printf("||");
     for (int i = 0; i < board -> size_r; i++){printf("=");}
@@ -171,42 +298,9 @@ void print_board(Board board){
 //starts the game
 void start_game(Board board){
     board = create_empty_board();
+    print_board(board);
     initialize_board(board, 2, 2);
     print_board(board);
 
-}
-
-
-void increase_number_of_neighbour_mines_for_neighbours(Board board, int r, int c){
-    //List:
-
-    // s0 s1 s2
-    // s3 rc s4
-    // s5 s6 s7
-
-    //s0 s1 s2 s3 s4 s5 s6 s7
-    //r0 r1 r2 r3 r4 r5 r6 r7
-    int *neighbour_rows_list = malloc(8 * sizeof(int));
-    //c0 c1 c2 c3 c4 c5 c6 c7
-    int *neighbour_columns_list = malloc(8 * sizeof(int));
-
-    // setting indexes
-    neighbour_rows_list[0] = neighbour_rows_list[1] = neighbour_rows_list[2] = r - 1;
-    neighbour_rows_list[3] = neighbour_rows_list[4] = r;
-    neighbour_rows_list[5] = neighbour_rows_list[6] = neighbour_rows_list[7] = r + 1;
-    neighbour_columns_list[0] = neighbour_columns_list[3] = neighbour_columns_list[5] = c - 1;
-    neighbour_columns_list[1] = neighbour_columns_list[6] = c;
-    neighbour_columns_list[2] = neighbour_columns_list[4] = neighbour_columns_list[7] = c + 1;
-
-    for (int i = 0; i < 8; i++){
-        //check if neighbours fit board
-        if(neighbour_rows_list[i] >= 0 &&
-        neighbour_rows_list[i] < board-> size_c &&
-        neighbour_columns_list[i] >= 0 &&
-        neighbour_columns_list[i] < board-> size_r){
-            //increase number of neighbouneighbour_rows_list, 8r mines
-            board -> squares[neighbour_rows_list[i] * board -> size_r + neighbour_columns_list[i]] -> number_of_neighbour_mines += 1;
-        }
-    }
 }
 
